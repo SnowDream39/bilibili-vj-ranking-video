26c6ba0ac5ebe1bf18953c9c98d9630dfe9021db
SnowDream39 <759723417@qq.com>
2024-08-01 11:27:59 +0800

    增加月回顾

---
+++ PS脚本/月回顾图片.jsx
@@ -0,0 +1,59 @@
+#include 'ps.jsx'
+
+
+function newImages() {
+    var fileRef = new File(currentFolder + "月回顾图片\\月回顾样式.psd");
+    if (fileRef.exists) {
+        app.open(fileRef);
+    } else {
+        $.writeln("文件不存在: " + fileRef);
+    }
+
+    // 确保有活动文档
+    if (app.documents.length > 0) {
+        var doc = app.activeDocument;
+        var layers = doc.layers;
+        var dataTodayNew = readJSONFile(currentFolder + "月回顾数据.json");
+
+        // 确保读取的 JSON 数据有效
+        if (dataTodayNew) {
+            for (var i = 0; i < dataTodayNew.length && i < 10; i++) {
+                var songData = dataTodayNew[i];
+
+                layers.getByName("新曲排名").textItem.contents = songData.day;
+                layers.getByName("得分").textItem.contents = comma(songData.point);
+
+                fillData(songData, layers.getByName("播放").layers, ["view", "viewR", "view_rank"]);
+                fillData(songData, layers.getByName("收藏").layers, ["favorite", "favoriteR", "favorite_rank"]);
+                fillData(songData, layers.getByName("硬币").layers, ["coin", "coinR", "coin_rank"]);
+                fillData(songData, layers.getByName("点赞").layers, ["like", "likeR", "like_rank"]);
+
+                setFormattedText(textLayer = layers.getByName("标题"), contents = songData.title, size = 60, font = "SourceHanSansSC-Bold", width=1330);
+                setFormattedText(textLayer = layers.getByName("作者"), contents = songData.author, size = 48, font = "SourceHanSansSC-Bold", width=650);
+                setFormattedText(textLayer = layers.getByName("歌手"), contents = songData.vocal, size = 36, font = "SourceHanSansCN-Bold", width=650);
+                setFormattedText(textLayer = layers.getByName("引擎"), contents = songData.synthesizer, size = 36, font = "SourceHanSansCN-Bold", width=650);
+
+                layers.getByName("BV号").textItem.contents = songData.bvid;
+                layers.getByName("投稿时间").textItem.contents = songData.pubdate.substring(0, 16);
+                layers.getByName("时长").textItem.contents = songData.duration;
+                if (songData.copyright === 1){
+                    var contents = "本家投稿";
+                } else {
+                    var contents = "搬运：" + songData.uploader;
+                }
+                setFormattedText(textLayer = layers.getByName("copyright"), contents = contents, size = 36, font = "SourceHanSansCN-Bold", width=350);
+
+                $.writeln('完成月回顾第' + (i+1) + "张图片");
+                savePic(doc, currentFolder + '月回顾图片\\' + (i+1) + ".png");
+            }
+
+            doc.close(SaveOptions.SAVECHANGES);
+        } else {
+            $.writeln("读取 JSON 数据时出错");
+        }
+    } else {
+        $.writeln("没有活动文档");
+    }
+}
+
+newImages();
--- 月回顾资源.py
+++ 月回顾资源.py
@@ -10,7 +10,7 @@ import httpx
 import yaml
 import os
 import shutil
-
+from math import ceil, floor
 
 contain = 20
 extend = 100
@@ -52,7 +52,7 @@ async def download_video(bvid):
     download_url_data = await v.get_download_url(0)
     # 解析视频下载信息
     detecter = video.VideoDownloadURLDataDetecter(data=download_url_data)
-    streams = detecter.detect_best_streams(video_max_quality=video.VideoQuality._1080P,audio_max_quality=video.AudioQuality._192K, codecs=[video.VideoCodecs.AVC, video.VideoCodecs.HEV, video.VideoCodecs.AV1])
+    streams = detecter.detect_best_streams()
     # 有 MP4 流 / FLV 流两种可能
     if detecter.check_flv_stream() == True:
         # FLV 流下载
@@ -67,7 +67,7 @@ async def download_video(bvid):
         await download_url(streams[1].url, "视频/audio_temp.m4s", "音频流")
         # 混流
         os.system(
-            f'{FFMPEG_PATH} -i 视频/video_temp.m4s -i 视频/audio_temp.m4s -vcodec copy -acodec copy 视频/{bvid}.mp4'
+            f"{FFMPEG_PATH} -i 视频/video_temp.m4s -i 视频/audio_temp.m4s -c:v libx264 -c:a aac -strict experimental 视频/{bvid}.mp4"
         )
         # 删除临时文件
         os.remove("视频/video_temp.m4s")
@@ -232,172 +232,44 @@ def delete_videos(days):
                 os.remove(f"视频/{old_video}.mp4")
 
 
-today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(
-    days=1
-)
-full_date = today.strftime("%Y.%m.%d")
-total_phase = (today - datetime(year=2024,month=7,day=2)).days
-themes = {
-    0: "周日主题色",
-    1: "周一主题色",
-    2: "周二主题色",
-    3: "周三主题色",
-    4: "周四主题色",
-    5: "周五主题色",
-    6: "周六主题色",
-}
-metadata = {
-    "full_date": today.strftime("%Y.%m.%d"),
-    "year": today.year,
-    "month": today.month,
-    "day": today.day,
-    "weekday": today.weekday()+1,
-    "theme": themes[(today.weekday()+1)%7],
-    "total_phase": total_phase,
-    "phase": total_phase
-}
+def update_algorithm(current_song_data):
+    if current_song_data.at[0,'copyright'] == 0:
+        current_song_data.at[0,'copyright'] = 2
+    view = current_song_data.at[0,"view"]
+    favorite = current_song_data.at[0,"favorite"]
+    coin = current_song_data.at[0,"coin"]
+    like = current_song_data.at[0,"like"]
+    copyright = current_song_data.at[0,"copyright"]
+    viewR = 0 if view == 0 else max(ceil(min(max((coin + favorite), 0) * 20 / view, 1) * 100) / 100, 0)
+    favoriteR = 0 if favorite <= 0 else max(ceil(min(max(favorite + 2 * coin, 0) * 10 / (favorite * 20 + view) * 40, 20) * 100) / 100, 0)
+    coinR = 0 if (1 if copyright in [1, 3] else 2) * coin * 40 + view == 0 else max(ceil(min(((1 if copyright in [1, 3] else 2) * coin * 40) / ((1 if copyright in [1, 3] else 2) * coin * 40 + view) * 80, 40) * 100) / 100, 0)
+    likeR = 0 if like <= 0 else max(floor(max(coin + favorite, 0) / (like * 20 + view) * 100 * 100) / 100, 0)
+    current_song_data['viewR'] = viewR
+    current_song_data['favoriteR'] = favoriteR
+    current_song_data['coinR'] = coinR
+    current_song_data['likeR'] = likeR
+    current_song_data['point'] = int(view * viewR + favorite * favoriteR + coin * coinR + like * likeR) 
 
-# 读取配置
+today = datetime.today()
+last_day_of_last_month = datetime(today.year,  today.month, 1)- timedelta(days=1)
+first_day_of_last_month = last_day_of_last_month.replace(day=1)
+current_day = first_day_of_last_month
+all_columns = ['title','bvid','name','author','uploader','copyright','synthesizer','vocal','pubdate','duration','view','favorite','coin','like','viewR','favoriteR','coinR','likeR','point']
+top_datas = pd.DataFrame(columns=all_columns)
 
-with open("基本配置.yaml", "r", encoding="utf-8") as file:
-    settings = yaml.safe_load(file)
-    contain = settings["contain"]
-    extend = settings["extend"]
-    new = settings["new"]
+while current_day < today:
+    filename = "月回顾数据/"+ current_day.strftime("%Y%m%d")+"与"+(current_day - timedelta(days=1)).strftime("%Y%m%d")+".xlsx"
+    current_song_data = pd.read_excel(filename,nrows=1,dtype={"pubdate": str})
+    if current_day < datetime(2024,7,12):
+        update_algorithm(current_song_data)
+    current_song_data = current_song_data[all_columns]
+    top_datas = pd.concat([top_datas, current_song_data],ignore_index=True)
+    current_day += timedelta(days=1)
 
-with open("每日配置.yaml", "r", encoding="utf-8") as file:
-    daily_preference = yaml.safe_load(file)[full_date]
+top_datas['view_rank'] = top_datas['view'].rank(ascending=False,method='min')
+top_datas['favorite_rank'] = top_datas['favorite'].rank(ascending=False,method='min')
+top_datas['coin_rank'] = top_datas['coin'].rank(ascending=False,method='min')
+top_datas['like_rank'] = top_datas['like'].rank(ascending=False,method='min')
 
-# 副榜BGM
-source_folder = r"D:\Music\VOCALOID传说曲"
-source_file = daily_preference["ED_filename"] + ".mp3"
-metadata["ED_title"] = daily_preference["ED_title"]
-source_path = os.path.join(source_folder, source_file)
-destination_file = "BGM/副榜.mp3"
-destination_path = os.path.join(os.getcwd(), destination_file)
-shutil.copyfile(source_path, destination_path)
-
-
-file_yesterday = (
-    today.strftime("%Y%m%d")
-    + "与"
-    + (today - timedelta(days=1)).strftime("%Y%m%d")
-    + ".xlsx"
-)
-file_today = (
-    (today + timedelta(days=1)).strftime("%Y%m%d")
-    + "与"
-    + today.strftime("%Y%m%d")
-    + ".xlsx"
-)
-
-file_new = (
-    "新曲"
-    + (today + timedelta(days=1)).strftime("%Y%m%d")
-    + "与新曲"
-    + today.strftime("%Y%m%d")
-    + ".xlsx"
-)
-
-songs_data_today = pd.read_excel("数据/" + file_today, dtype={"pubdate": str})
-songs_data_today = songs_data_today.head(extend)
-songs_data_today["pic"] = songs_data_today["bvid"] + ".png"
-songs_data_before = pd.read_excel("数据/" + file_yesterday, dtype={"pubdate": str})
-songs_data_new = pd.read_excel("数据/" + file_new, dtype={"pubdate": str})
-songs_data_new["rank"]
-songs_data_new = songs_data_new.head(new)
-
-metadata["OP_bvid"] = songs_data_before.at[0, "bvid"]
-metadata["OP_title"] = songs_data_before.at[0, "title"]
-metadata["OP_author"] = songs_data_before.at[0, "author"]
-
-
-with open("截取片段.json", "r", encoding="utf-8") as file:
-    clip_points = json.load(file)
-
-downloaded_videos = os.listdir("./视频")
-pics_filename = os.listdir("./封面")
-pics_bvid = [pic[0:12] for pic in pics_filename]
-pics = {bvid: filename for bvid, filename in zip(pics_bvid, pics_filename)}
-
-# 加入其他数据
-insert_before(songs_data_today, songs_data_before)
-
-# 判断作者名字的语言，待完善
-songs_data_new["author_language"] = "default"
-for i in songs_data_new.index:
-    author = str(songs_data_new.at[i, "author"])
-    for character in author:
-        if detect_language(character) == "Korean":
-            songs_data_new.at[i, "author_language"] = "Korean"
-            break
-
-songs_data_today["author_language"] = "default"
-for i in songs_data_today.index:
-    author = songs_data_today.at[i, "author"]
-    for character in author:
-        if detect_language(character) == "Korean":
-            songs_data_today.at[i, "author_language"] = "Korean"
-            break
-
-# 下载封面
-for i in range(extend):
-    bvid = songs_data_today.at[i, "bvid"]
-    if bvid not in pics.keys():
-        asyncio.get_event_loop().run_until_complete(download_thumbnail(bvid))
-
-
-bvid = daily_preference.get("thumbnail", songs_data_new.at[0, "bvid"])
-metadata["thumbnail"] = bvid + ".png"
-with open("基本信息数据.json", "w", encoding="utf-8") as file:
-    json.dump(metadata, file, ensure_ascii=False, indent=4)
-asyncio.get_event_loop().run_until_complete(download_thumbnail_special(bvid))
-
-songs_data_today.to_json("数据.json", force_ascii=False, orient="records", indent=4)
-songs_data_new.to_json("新曲数据.json", force_ascii=False, orient="records", indent=4)
-print("现在可以开始制作图片")
-
-
-# 删除过期视频
-today_videos = []
-for i in range(contain):
-    bvid = songs_data_today.at[i, "bvid"]
-    today_videos.append(bvid)
-delete_videos(2)
-delete_videos(20)
-
-
-# 关于新曲的文件
-file_path = f"视频/{today.strftime('%Y%m%d')}下载视频.json"
-if os.path.exists(file_path):
-    with open(file_path, "r") as file:
-        download_list = json.load(file)
-else:
-    download_list = []
-
-# 找下载新曲视频
-insert_clip_points(songs_data_new, clip_points, new)
-for i in range(new):
-    bvid = songs_data_new.at[i, "bvid"]
-    if bvid + ".mp4" not in downloaded_videos and bvid not in download_list:
-        download_list.append(bvid)
-
-# 找下载主榜视频
-insert_clip_points(songs_data_today, clip_points, contain)
-for i in range(contain):
-    bvid = songs_data_today.at[i, "bvid"]
-    if bvid + ".mp4" not in downloaded_videos and bvid not in download_list:
-        download_list.append(bvid)
-
-file_path = f"视频/{today.strftime('%Y%m%d')}下载视频.json"
-with open(file_path, "w") as file:
-    json.dump(download_list, file, ensure_ascii=False, indent=4)
-
-total = len(download_list)
-for i in range(total):
-    bvid = download_list[i]
-    if bvid + ".mp4" not in downloaded_videos:
-        asyncio.get_event_loop().run_until_complete(download_video(bvid=bvid))
-        print(f"下载进度：{i+1}/{total}")
-
-print("现在清您去截取片段")
+top_datas.to_excel("月回顾数据.xlsx",index=False)
+top_datas.to_json("月回顾数据.json", force_ascii=False, orient="records", indent=4)
