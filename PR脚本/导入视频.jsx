cf05f62e92d2c0e53fb249f0297cffa0aac7fbcd
SnowDream39 <759723417@qq.com>
2024-11-05 13:56:28 +0800

    整合特刊

---
+++ PR脚本/导入视频.jsx
@@ -0,0 +1,55 @@
+#include 'pr.jsx'
+
+var sequenceName = "特刊";
+var project = app.project;
+var rootItem = project.rootItem;
+
+var sequences = project.sequences;
+for (var i = 0; i < sequences.length; i++) {
+    project.deleteSequence(sequences[i]);
+}
+var sequence = project.createNewSequence(sequenceName, "standard");
+
+var audioTracks = sequence.audioTracks;
+var musicTrack = audioTracks[0];
+var songTrack = audioTracks[1];
+var videoTracks = sequence.videoTracks;
+var imageTrack = videoTracks[0];
+var videoTrack = videoTracks[1];
+
+var cutBin = refreshBin(rootItem, '截取片段用视频');
+var monthReviewImageBin = refreshBin(rootItem, '月回顾图片');
+var monthReviewVideoBin = refreshBin(rootItem, '月回顾视频');
+
+data = readJSONFile(currentFolder + "视频列表.json")
+points = readJSONFile(currentFolder + "截取片段.json")
+
+var lengths = [];
+var imageFiles = [];
+var videoFiles = [];
+
+
+project.importFiles(videoFiles,false, monthReviewImageBin,false)
+for (var i = 0; i < data.length; i++) {
+    var bvid = data[i].bvid;
+    filepath = currentFolder + "视频\\" + bvid + ".mp4";
+    if (videoFiles.indexOf(filepath) === -1){
+        videoFiles.push(filepath);
+    }
+}
+project.importFiles(videoFiles,false, monthReviewVideoBin,false)
+
+
+var videoItems = monthReviewVideoBin.children;
+for(var i=0; i<videoItems.length; i++){
+    var item = videoItems[i];
+    bvid = item.name.split('.')[0];
+    item.setInPoint(jsonGet(points, 'bvid', bvid).inPoint,4);
+    item.setOutPoint(jsonGet(points, 'bvid', bvid).outPoint,4);
+    item.name = jsonGet(data, 'bvid', bvid).belongs + jsonGet(data, 'bvid', bvid).rank
+
+}
+
+
+project.save();
+
---
+++ PS脚本/特刊图片.jsx
@@ -0,0 +1,47 @@
+#include 'ps.jsx'
+
+
+function newImages() {
+    var fileRef = new File(currentFolder + "主榜图片\\梦的结唱样式2.psd");
+    if (fileRef.exists) {
+        app.open(fileRef);
+    } else {
+        $.writeln("文件不存在: " + fileRef);
+    }
+
+    // 确保有活动文档
+    if (app.documents.length > 0) {
+        var doc = app.activeDocument;
+        var layers = doc.layers;
+        var data = readJSONFile(currentFolder + "数据.json");
+
+        // 确保读取的 JSON 数据有效
+        if (data) {
+            for (var i = 0; i < 30; i++) {
+                var songData = data[i];
+
+                layers.getByName("排名").textItem.contents = songData.rank;
+                layers.getByName("得分").textItem.contents = comma(songData.point);
+                layers.getByName("总补正").textItem.contents = '×' + songData.fix.toFixed(2) + ' =';
+
+                fillSongInfo(layers.getByName("歌曲信息").layers, songData)
+
+                fillData(songData, layers.getByName("播放").layers, ["view", "viewR", "view_rank"]);
+                fillData(songData, layers.getByName("收藏").layers, ["favorite", "favoriteR", "favorite_rank"]);
+                fillData(songData, layers.getByName("硬币").layers, ["coin", "coinR", "coin_rank"]);
+                fillData(songData, layers.getByName("点赞").layers, ["like", "likeR", "like_rank"]);
+
+                $.writeln('完成第' + (i+1) + "张图片");
+                savePic(doc, currentFolder + '翻唱榜\\' + (songData.rank) + ".png");
+            }
+
+            // doc.close(SaveOptions.SAVECHANGES);
+        } else {
+            $.writeln("读取 JSON 数据时出错");
+        }
+    } else {
+        $.writeln("没有活动文档");
+    }
+}
+
+newImages();
